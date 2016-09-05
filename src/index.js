60ec3ec1e5ad1cb9dbca757c8f72cd678309e087
hosseintoussi <hossein@mindvalley.com>
2016-09-05 09:34:07 +0800

    react youtubve tutorial

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+import YTSearch from 'youtube-api-search'
+const API_KEY = 'AIzaSyCyjOhupg8ZWUwQAVpZuYd6FxupGoJjHYo';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedVideo: null,
+      videos: []
+    };
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ videos: videos,
+                      selectedVideo: videos[0] });
+        // { videos: videos }
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+    return (
+        <div >
+        <SearchBar onSearchTermChange={ videoSearch }/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+        onVideoSelect = { selectedVideo => this.setState({ selectedVideo }) }
+        videos={this.state.videos} />
+        </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
